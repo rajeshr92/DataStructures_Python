f885396f1db16b50473e2d5bea76b32070eadbe1
Unknown <rajeshr92@gmail.com>
2018-11-18 19:28:46 -0800

    Added Doc string
    add comments

--- Graph_Implementation_Using_LinkedList.py
+++ Graph_Implementation_Using_LinkedList.py
@@ -1,14 +1,23 @@
-from LinkedList import LinkedList
+''' 
+Graph can be represented in many forms. While the problem statement determines the best form of repsentation, the code below represents graph using a Linked List data structure. 
+The code also has methods to create, add, and print elements and edges in the bi-directional graph
+'''
+
+from LinkedList import LinkedList ''' Python syntax to inherit other classes '''
 
 class graph:
     
     def __init__(self):
         self.dic = {}
-    
+
+    ''' createDic method will assign objects of the LinkedList class i.e. temp to each parent node i.e. key in the dictionary '''    
+
     def createDic(self,v):
         temp = LinkedList()
         self.dic[v] = temp
         return
+
+    ''' Add_Edge is a method to add elements to the graph for a given parent node using the methods within the LinkedList() class '''
     
     def Add_Edge(self,u,v):
         self.dic[u].insertAthead(v)
