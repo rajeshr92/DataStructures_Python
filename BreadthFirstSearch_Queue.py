fbb1d8ad3b3c2aa901e5b6f1cf6251269dab4373
Unknown <rajeshr92@gmail.com>
2018-11-18 18:55:51 -0800

    Added commented doc string
    doc string

--- BreadthFirstSearch_Queue.py
+++ BreadthFirstSearch_Queue.py
@@ -1,29 +1,29 @@
-/*
+'''
 The following is a breadth first implementation using queue data strcuture. 
 
 'bfs' function accepts two arguments; a graph represented in the form of a dictionary and a starting point for the graph to start the breadth first search 
-*/
+'''
 
 def bfs(graph, StartPoint):
 
-    q = [StartPoint] /* a list that is initilized with the starting point */
-    path = [] /* an empty list to document the path as the program traverses through the graph */ 
+    q = [StartPoint] ''' a list that is initilized with the starting point '''
+    path = [] ''' an empty list to document the path as the program traverses through the graph '''
 
     while q:
 
         current = q.pop()
 
-        path.append(current) /* appending the element to path after being visited */
+        path.append(current) '''appending the element to path after being visited'''
 
         for i in graph[current]:
             if i not in q:
-                q.insert(0,i) /* taking elements/integers from the graph and queueing it to the list 'q' only after checking if it is a parent node that has already been visited */
+                q.insert(0,i) '''taking elements/integers from the graph and queueing it to the list 'q' only after checking if it is a parent node that has already been visited'''
 
     return path
 
 
 
-/* The below is a test case where integers in the form of a graph are passed to the bfs function as a dictionary */
+'''The below is a test case where integers in the form of a graph are passed to the bfs function as a dictionary'''
 
 adjacency_matrix = {1: [2, 3], 2: [4, 5],
                     3: [5], 4: [6], 5: [6],
